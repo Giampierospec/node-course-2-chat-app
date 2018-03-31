88e1639e133518d47b49f96b768775d97a44fd45
Giampiero Specogna <giampi_12@hotmail.com>
2018-03-31 11:06:27 -0400

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,20 +5,39 @@ const path = require('path');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 console.log(publicPath);
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 const {generateMessage,generateLocationMessage} = require('./utils/message');
 io.on('connection',(socket)=>{
+    socket.on('join',(params, callback)=>{
+        if(!isRealString(params.name) || !isRealString(params.room))
+            return callback('Name and roomname are required');
+        else{
+            socket.join(params.room);
+            users.removeUser(socket.id);
+            users.addUser(socket.id, params.name,params.room);
+            io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+            socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+            socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',`${params.name} has joined`));
+            callback();
+        }
+           
+    });
     console.log('New User Connected');
     socket.on('disconnect',()=>{
-        console.log('Client disconnected');
+        var user = users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+            io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left.`));
+        }
+
     });
-    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
     socket.on('createMessage',(msg,callback)=>{
         
         io.emit('newMessage',generateMessage(msg.from, msg.text));
